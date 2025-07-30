b60fd0f5d8d1d0a2a2af4c63f35fc1f45b06e1a3
afterforever17 <pbggabi@gmail.com>
2025-07-30 19:30:18 +0300

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,14 @@
+using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Entities;
 
 public class AppUser
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    public required string Display { get; set; }
+    public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 
 }
